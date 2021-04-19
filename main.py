20437e8e6cd9c9f56df48a96b252b81b863b17c8
Adam Sokolowski <adisok@wp.pl>
2021-04-19 18:03:54 +0200

    Trying to use Jet Brains git plugin

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "Hello world!"}
+
+@app.get("/method")
+def get_method():
+    return {"method": "GET"}
+
 
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from fastapi.testclient import TestClient
 from main import app
+import pytest
 
 client = TestClient(app)
 
@@ -7,3 +8,9 @@ def test_read_main():
     resposne = client.get("/")
     assert resposne.status_code == 200
     assert resposne.json() == {"message": "Hello World"}
+
+@pytest.mark.parametrize("name", ["Zenek", "Marek", "Alojzy Niezdąży"])
+def test_hello_name(name):
+    response = client.get(f"/hello/{name}")
+    assert response.status_code == 200
+    assert response.text == f'"Hello {name}"'
