0436d3ae7ba8c8e2ae4bbc13d23aa2d4b0d3fe24
Adam Sokolowski <adisok@wp.pl>
2021-04-19 19:23:20 +0200

    DaftAcademy EX. 1.3

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI, Response, status
+from fastapi import FastAPI, Response, status, Query
+from passlib.hash import sha512_crypt
+import hashlib
+import requests
 
 app = FastAPI()
 
@@ -7,28 +10,34 @@ def root():
     return {"message": "Hello world!"}
 
 
-@app.get("/method", status_code=200)
+@app.get("/method")
 def get_method(response: Response):
     response.status_code = status.HTTP_200_OK
     return {"method": "GET"}
 
-@app.options("/method", status_code=200)
+@app.options("/method")
 def get_method(response: Response):
     response.status_code = status.HTTP_200_OK
     return {"method": "OPTIONS"}
 
-@app.put("/method", status_code=200)
+@app.put("/method")
 def get_method(response: Response):
     response.status_code = status.HTTP_200_OK
     return {"method": "PUT"}
 
-@app.delete("/method", status_code=200)
+@app.delete("/method")
 def get_method(response: Response):
     response.status_code = status.HTTP_200_OK
     return {"method": "DELETE"}
 
-@app.post("/method", status_code=201)
+@app.post("/method")
 def get_method(response: Response):
     response.status_code = status.HTTP_201_CREATED
     return {"method": "POST"}
 
+@app.get("/auth")
+def chech_hash_password(response: Response, password: str = Query(None), password_hash: str = Query(None)):
+    if hashlib.sha512(bytes(password, 'ascii')).hexdigest() == password_hash:
+        response.status_code = status.HTTP_204_NO_CONTENT
+    else:
+        response.status_code = status.HTTP_401_UNAUTHORIZED
--- test.py
+++ test.py
@@ -13,3 +13,12 @@ def test_method():
     response = client.get("/method")
     assert response.status_code == 200
     assert response.json() == {"method": "GET"}
+
+def test_hash_password():
+    params = {
+        "password": "haslo",
+        "password_hash": "013c6889f799cd986a735118e1888727d1435f7f623d05d58c61bf2cd8b49ac90105e5786ceaabd62bbc27336153d0d316b2d13b36804080c44aa6198c533215"
+    }
+    response = client.get("/auth", params=params)
+    assert response.status_code == 204
+    print(response.text)
