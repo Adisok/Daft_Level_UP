2bbb1462e3a2671a0eaeb2836f54c1912427c49e
Adam Sokolowski <adisok@wp.pl>
2021-04-29 23:34:17 +0200

    Daft EX 3.2

--- main.py
+++ main.py
@@ -90,7 +90,9 @@ def hello_html(request: Request):
 def login_session(username: str, password: str, response: Response):
     user = "4dm1n"
     pas = "NotSoSecurePa$$"
-    if username == user and password == pas:
+    session_token = hashlib.sha256(f"{user}{pas}".encode()).hexdigest()
+    check_token = hashlib.sha256(f"{username}{password}".encode()).hexdigest()
+    if session_token == check_token:
         session_token = hashlib.sha256(f"{user}{pas}".encode()).hexdigest()
         app.tokens.append(session_token)
         response.set_cookie(key="session_token", value=session_token)
