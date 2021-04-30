4c0a973be80aed52314d2aaa35bc41571e13687a
Adam Sokolowski <adisok@wp.pl>
2021-05-01 01:52:44 +0200

    Daft EX 3.2

--- main.py
+++ main.py
@@ -94,21 +94,28 @@ def login_session(response: Response, username: str = "", password: str = ""):
     check_token = hashlib.sha256(f"{username}{password}".encode()).hexdigest()
 
     if session_token == check_token:
-        session_token = hashlib.sha256(f"{user}{pas}".encode()).hexdigest()
         app.token = session_token
         response.set_cookie(key="session_token", value=session_token)
         response.status_code = status.HTTP_201_CREATED
         return {"session_token": session_token}
 
     if session_token != check_token:
+        response.delete_cookie(key="session_token")
+        app.token = ""
+        response.set_cookie(key="session_token", value=check_token)
         raise HTTPException(status_code=401, detail="Wrong Passowrd or Username")
 
 
 @app.post("/login_token")
 def login_token(*, response: Response, session_token: str = Cookie(None)):
-    if session_token == app.token:
+    if session_token != app.token:
+        raise HTTPException(status_code=401, detail="Wrong Passowrd or Username")
+    else:
         response.set_cookie(key="token_value", value=session_token)
         response.status_code = status.HTTP_201_CREATED
         return {"token": f"{session_token}"}
-    if session_token != app.token:
-        raise HTTPException(status_code=401, detail="Wrong Passowrd or Username")
+
+
+# @app.get("/welcome_session")
+#
+# @app.get("/welcome_token")
