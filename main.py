2b47fffd738440bf361337075e6275e47be49604
Adam Sokolowski <adisok@wp.pl>
2021-05-01 02:51:30 +0200

    Daft EX 3.2

--- main.py
+++ main.py
@@ -90,28 +90,30 @@ def hello_html(request: Request):
 def login_session(response: Response, username: str = "", password: str = ""):
     user = "4dm1n"
     pas = "NotSoSecurePa$$"
+    session_token = hashlib.sha256(f"{user}{pas}".encode()).hexdigest()
+    check_token = hashlib.sha256(f"{username}{password}".encode()).hexdigest()
 
-    if user == username and pas == password:
-        session_token = hashlib.sha256(f"{username}{password}".encode()).hexdigest()
+    if session_token == check_token:
+        app.token = session_token
         response.set_cookie(key="session_token", value=session_token)
         response.status_code = status.HTTP_201_CREATED
-    else:
+        #return {"session_token": session_token}
+
+    if session_token != check_token:
         response.delete_cookie(key="session_token", path="/login_session")
+        app.token = ""
+        response.set_cookie(key="session_token", value=check_token)
         raise HTTPException(status_code=401, detail="Wrong Passowrd or Username")
 
 
 @app.post("/login_token")
-def login_token(*, response: Response, username: str = "", password: str = ""):
-    user = "4dm1n"
-    pas = "NotSoSecurePa$$"
-
-    if user == username and pas == password:
-        response.status_code = status.HTTP_201_CREATED
-        token = hashlib.sha256(f"{username}{password}".encode()).hexdigest()
-        return {"token": f"{token}"}
-    else:
+def login_token(*, response: Response, session_token: str = Cookie(None)):
+    if session_token != app.token:
         raise HTTPException(status_code=401, detail="Wrong Passowrd or Username")
-
+    else:
+        response.set_cookie(key="token_value", value=session_token)
+        response.status_code = status.HTTP_201_CREATED
+        return {"token": f"{session_token}"}
 
 
 # @app.get("/welcome_session")
