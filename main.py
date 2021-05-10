36ebf8a7b2d3c18d463e07d156168b373842f69d
Adam Sokolowski <adisok@wp.pl>
2021-05-10 22:03:31 +0200

    Daft EX 4.3

--- main.py
+++ main.py
@@ -236,8 +236,8 @@ async def get_products():
 
     products_info = app.db_connection.execute(
         "SELECT Products.ProductID AS id, Products.ProductName AS name, Categories.CategoryName AS category, "
-        " Suppliers.CompanyName AS supplier FROM Products JOIN Categories ON Products.ProductID = Categories.CategoryID "
-        "JOIN Suppliers ON Products.SupplierID = Suppliers.SupplierID"
+        " Suppliers.CompanyName AS supplier FROM Products JOIN Categories ON Products.CategoryID = Categories.CategoryID "
+        "JOIN Suppliers ON Products.SupplierID = Suppliers.SupplierID ORDER BY Products.ProductsID"
     ).fetchall()
 
     return {
